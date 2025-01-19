0c01b80c399b24653788483c7c0e157143f3dd0e
Nazym <nazymtolebek@gmail.com>
2025-01-19 20:53:33 +0500

    дурыс ураа

--- 175.py
+++ 175.py
@@ -1,2 +1,7 @@
 # https://leetcode.com/problems/combine-two-tables/description/
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    result = pd.merge(person, address, how='left', on='personId')
+    return result[['firstName', 'lastName', 'city', 'state']]
 
--- 176.py
+++ 176.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     employee = employee['salary'].drop_duplicates()
-    employee.sort_values(ascending=False)
-    if len(employee) == 1:
+    employee = employee.sort_values(ascending=False)
+    if len(employee) <= 1:
         return pd.DataFrame({'SecondHighestSalary' : [None]})
     second_salary = employee.iloc[1]
     return pd.DataFrame({'SecondHighestSalary' : [second_salary]})
